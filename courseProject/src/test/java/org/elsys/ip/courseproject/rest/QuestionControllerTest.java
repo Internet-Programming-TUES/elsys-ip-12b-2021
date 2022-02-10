7c02c2071568461802700689bfd07937e615f44c
Ivaylo Mihaylov <ivaylo.mihaylov@botronsoft.com>
2022-02-10 09:47:33 +0200

    DirtiesContext

--- courseProject/src/test/java/org/elsys/ip/courseproject/rest/QuestionControllerTest.java
+++ courseProject/src/test/java/org/elsys/ip/courseproject/rest/QuestionControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -21,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class QuestionControllerTest {
 
     @LocalServerPort
@@ -50,8 +52,10 @@ public class QuestionControllerTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
-    @BeforeAll
+    @BeforeEach
     public void setUp() {
+        assertThat(repo.count()).isEqualTo(0);
+
         Question question = new Question();
         question.setText("Kolko e 2+2?");
         Answer answer1 = new Answer();
