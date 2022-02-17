2972ae33b8af14fe8f175ee6c27b3eefcb16dbf3
Ivaylo Mihaylov <ivaylo.mihaylov@botronsoft.com>
2022-02-17 09:40:48 +0200

    Mapping.

--- courseProject/src/main/java/org/elsys/ip/courseproject/rest/QuestionController.java
+++ courseProject/src/main/java/org/elsys/ip/courseproject/rest/QuestionController.java
@@ -5,13 +5,15 @@ import org.elsys.ip.courseproject.model.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@RestController("/question")
+@RestController
+@RequestMapping("/question")
 public class QuestionController {
     private final QuestionRepository repository;
 
