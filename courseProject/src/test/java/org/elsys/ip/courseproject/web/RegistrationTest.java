397041ddc067cdc6e8d531f2e9298329897e7788
Ivaylo Mihaylov <ivaylo.mihaylov@botronsoft.com>
2022-03-17 09:49:17 +0200

    Validation on registration.

--- courseProject/src/test/java/org/elsys/ip/courseproject/web/RegistrationTest.java
+++ courseProject/src/test/java/org/elsys/ip/courseproject/web/RegistrationTest.java
@@ -1,5 +1,7 @@
 package org.elsys.ip.courseproject.web;
 
+import org.assertj.core.util.Lists;
+import org.assertj.core.util.Sets;
 import org.elsys.ip.courseproject.web.pageobjects.HomePage;
 import org.elsys.ip.courseproject.web.pageobjects.LoginPage;
 import org.elsys.ip.courseproject.web.pageobjects.RegistrationPage;
@@ -8,15 +10,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.elsys.ip.courseproject.web.selenium.SeleniumConfig.delay;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -42,8 +44,40 @@ public class RegistrationTest {
     public void registration() {
         HomePage homePage = HomePage.open(webDriver, port);
         RegistrationPage registrationPage = homePage.openRegistration();
-        LoginPage loginPage = registrationPage.register("First Name", "Last Name", "email@email.com", "qwerty");
+        LoginPage loginPage = registrationPage.validRegister("First Name", "Last Name", "email@email.com", "qwerty");
         homePage = loginPage.login("email@email.com", "qwerty");
         assertThat(homePage.isAuthenticated()).isTrue();
+    }
+
+    @Test
+    public void registrationValidation() {
+        HomePage homePage = HomePage.open(webDriver, port);
+        RegistrationPage registrationPage = homePage.openRegistration();
+        registrationPage.register("", "", "", "", "1");
+        registrationPage.assertErrors(
+                Set.of("must not be empty"),
+                Set.of("must not be empty"),
+                Set.of("must not be empty", "Invalid email"),
+                Set.of("must not be empty"),
+                Set.of("Passwords don't match")
+        );
+
+        registrationPage.register("First Name", "", "", "", "1");
+        registrationPage.assertErrors(
+                Sets.newHashSet(),
+                Set.of("must not be empty"),
+                Set.of("must not be empty", "Invalid email"),
+                Set.of("must not be empty"),
+                Set.of("Passwords don't match")
+        );
+
+        registrationPage.register("First Name", "Last Name", "email", "qwerty", "qwerty");
+        registrationPage.assertErrors(
+                Sets.newHashSet(),
+                Sets.newHashSet(),
+                Set.of("Invalid email"),
+                Sets.newHashSet(),
+                Sets.newHashSet()
+        );
     }
 }
--- courseProject/src/test/java/org/elsys/ip/courseproject/web/pageobjects/RegistrationPage.java
+++ courseProject/src/test/java/org/elsys/ip/courseproject/web/pageobjects/RegistrationPage.java
@@ -4,43 +4,104 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.springframework.security.core.parameters.P;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RegistrationPage extends AbstractPage {
-    @FindBy(id="firstName")
+    @FindBy(id = "firstName")
     WebElement firstName;
 
-    @FindBy(id="lastName")
+    @FindBy(id = "lastName")
     WebElement lastName;
 
-    @FindBy(id="email")
+    @FindBy(id = "email")
     WebElement email;
 
-    @FindBy(id="password")
+    @FindBy(id = "password")
     WebElement password;
 
-    @FindBy(id="matchingPassword")
+    @FindBy(id = "matchingPassword")
     WebElement matchingPassword;
 
     @FindBy(tagName = "button")
     WebElement button;
 
+    @FindBy(css = "#firstName ~ .error")
+    List<WebElement> firstNameErrors;
+
+    @FindBy(css = "#lastName ~ .error")
+    List<WebElement> lastNameErrors;
+
+    @FindBy(css = "#email ~ .error")
+    List<WebElement> emailErrors;
+
+    @FindBy(css = "#password ~ .error")
+    List<WebElement> passwordErrors;
+
+    @FindBy(css = "form > .error")
+    List<WebElement> globalErrors;
+
     public RegistrationPage(WebDriver webDriver) {
         super(webDriver);
     }
 
-    public LoginPage register(
+    public LoginPage validRegister(
             String firstName,
             String lastName,
             String email,
             String password
     ) {
+        register(firstName, lastName, email, password, password);
+
+        return PageFactory.initElements(webDriver, LoginPage.class);
+    }
+
+    public void register(
+            String firstName,
+            String lastName,
+            String email,
+            String password,
+            String matchingPassword
+    ) {
         this.firstName.sendKeys(firstName);
         this.lastName.sendKeys(lastName);
         this.email.sendKeys(email);
         this.password.sendKeys(password);
-        this.matchingPassword.sendKeys(password);
+        this.matchingPassword.sendKeys(matchingPassword);
         button.click();
+    }
 
-        return PageFactory.initElements(webDriver, LoginPage.class);
+    public void assertErrors(
+            Set<String> firstNameErrors,
+            Set<String> lastNameErrors,
+            Set<String> emailErrors,
+            Set<String> passwordErrors,
+            Set<String> globalErrors
+    ) {
+        assertThat(
+                this.firstNameErrors.stream().map(x -> x.getText())
+                        .collect(Collectors.toSet()).equals(
+                                firstNameErrors)).isTrue();
+        assertThat(
+                this.lastNameErrors.stream().map(x -> x.getText())
+                        .collect(Collectors.toSet())
+                        .equals(lastNameErrors)).isTrue();
+        assertThat(
+                this.emailErrors.stream().map(x -> x.getText())
+                        .collect(Collectors.toSet())
+                        .equals(emailErrors)).isTrue();
+        assertThat(
+                this.passwordErrors.stream().map(x -> x.getText())
+                        .collect(Collectors.toSet())
+                        .equals(passwordErrors)).isTrue();
+        assertThat(
+                this.globalErrors.stream().map(x -> x.getText())
+                        .collect(Collectors.toSet())
+                        .equals(globalErrors)).isTrue();
     }
 }
